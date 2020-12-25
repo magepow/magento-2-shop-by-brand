66a15ba83dc7797218c912c1080ecf43b39eedbb
Alex Dong <support@alothemes.com>
2020-12-25 13:43:41 +0700

    Clean code

---
+++ Helper/Alothemes.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @Author: Alex Dong
+ * @Date:   2020-12-25 09:16:34
+ * @Last Modified by:   Alex Dong
+ * @Last Modified time: 2020-12-25 09:17:05
+ */
+
+namespace Magiccart\Shopbrand\Helper;
+
+class Alothemes extends Data
+{
+    public function getTimer($_product)
+    {
+
+    }
+
+    public function getLabels($_product)
+    {
+
+    }
+}
